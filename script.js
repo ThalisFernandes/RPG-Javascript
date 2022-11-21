4780c3590480c71575e21f65720370073509018c
ThalisFernandes <thalisfernandesrodrigues@gmail.com>
2022-11-21 17:05:03 -0300

    feat: criando o script por fora para a intro do game removendo a intro do loop principal

--- script.js
+++ script.js
@@ -52,200 +52,7 @@ var introCount = 0;
  *  19 little lake       
  * 
  */
-let gameIntro = [
-	"No começo tudo era apenas trevas...",
 
-"e os Deuses viviam no vazio...",
-
-"até que uma grande luz surgiu do\n abismo de trevas...",
-
-"tranzendo consigo algo... a VIDA...",
-
-"os Deuses olharam a explosão de luz que gerou o universo...",
-
-"porém, eles não sabiam oque fazer após aquela explosão...",
-
-"no meio de tudo isso um pequeno planeta...",
-
-"sozinho no extremo canto direito, ao fim de todo o espaço...",
-
-"com sua própria lua, e um sol, chamou a atenção dos Deuses...",
-
-"pois este planeta tinha algo de especial...",
-
-"os Deuses tiveram uma ideia", 
-
-"criar uma raça, para viver nesse planeta...",
-
-"Alhtrora a Deusa da sabedoria, concedeu a", 
-
-"essas criaturas capacidade de pensar...",
-
-"drauthor o Deus da força e virtude, concedeu a essa raça, força... ",
-
-"zuldazar o Deus das florestas, concedeu a eles", 
-
-"habilidade de se comunicar com a floresta...",
-
-"Therathor o Criador Divino, os ensinou a criatividade... ",
-
-"Lathare o Deus dos ventos, fez com que ",
-
-"os ventos ficassem agradaveis e não muito fortes...",
-
-"Drathur-alor Deus dos mares, concedeu", 
-
-"sabedoria para poderem velejar e criar embarcações...",
-
-"para que assim eles pudessem desbravar o planeta deles..",
-
-"Arth-azarir-dratur Filha de Alhtrora e Drauthor", 
-
-"os concedeu a capacidade de amar uns aos outros...",
-
-"Os demais Deuses evitaram ter contato com a raça criada...",
-
-"que foi chamada de \"Os filhos\" ...",
-
-"os Filhos viveram 5000 anos, desbravaram", 
-
-"o planeta, criaram outras tribos...",
-
-"criaram reinos e cidades ...",
-
-"no ano 5000 a.R, o \"Filho\" chamado", 
-
-"Ultrierl, filho de UrtGarzelg e MirthGranzirdth...",
-
-"contra o conselho dos dez, o governo liderado", 
-
-"pelos herdeiros dos \"Primeiros Filhos\"...",
-
-"Ultrierlt liderou um grupo para viverem", 
-
-"no continente de Dralzar...",
-
-"um continent desertico, cheio de  vulcões", 
-
-"e  cercado pelo deserto que ficou conhecido como", 
-
-"\"deserto do desespero\"...",
-
-"Onde diversos \"filhos\", se mataram por não", 
-
-"verem esperança ao tentar atravesar o deserto ...",
-
-"Ultrierlt criou a cidade de VulDrathug, conhecida", 
-
-"como a cidade dos filhos de Ultrierlt...",
-
-"no ano de 3000 a.R, se iniciou a primeira guerra dos filhos...",
-
-"uma guerra que separou ainda mais os povos,", 
-
-"fazendo com que os filhos de Ultrierlt...",
-
-"se auto intitulassem de \"os Ultriem\"", 
-
-"suas peles após anos expostas ao calor...",
-
-"e vivendo perto de enorme vulcão, começou a escurecer...",
-
-"e os filhos começaram a não ver mais os Ultriem como naturais...",
-
-"no ano de 2000 a.R, outra divisão ocorreu,", 
-
-"Filhos que começaram a se envolver com necromancia...",
-
-"e magias profanas aos Deuses, ",
-
-"foram explusos da escola de Magia de DrunDahlMor,", 
-
-"e foram chamados de \"ZulDamor\"...",
-
-"os ZulDamor conseguiram contato ",
-
-"com uma criatura ancestral chamada Zul'thum, o senhor da escuridão...",
-
-"Zul'thum profanou os ZulDamor", 
-
-"fazendo com que crescecem chifres em suas cabeças...",
-
-"explusos e agora transformados,", 
-
-"eles se exilaram para uma ilha muito distante, chamada de Zaltror...",
-
-"uma ilha com criaturas ferrozes e seres das", 
-
-"trevas escondidos dentro de suas cavernas...",
-
-"nessa ilha eles criaram o templo de zul'thum...",
-
-"no ano 1501 a.R, os Ultriem começaram a negociar com os Zuldamor....",
-
-"no Ano 500 a.R, nasceu Iksu Darthu'zaldor", 
-
-"filho de Naena Zaltrugir (Zuldamor) e Naramos TheraDhur'zal(Ultriem)...",
-
-"Iksu, começou um levante aos 20 anos,", 
-
-"incitando os exercitos para que eles pudessem se rebelar contra os filhos ...",
-
-"no ano de 350 a.R Iksu se encontrou com Zul'thum no templo", 
-
-"onde o mesmo o nomeou o comandante dos exercitos ddas sombras...",
-
-"no ano 100 a.R as tropas dos Zuldamor e Ultriem,", 
-
-"começaram a se preparar para a grande guerra...",
-
-"no ano 50 a.R, Zul'thum pediu que fosse criado", 
-
-"um enorme portal chamado de Zal'taraut,", 
-
-"para que assim ele pudesse sair do abismo das trevas ...",
-
-"no ano 10 a.R os Zuldamor e Ultriem", 
-
-"iniciaram a guerra chamada de Rebelião das Trevas...",
-
-"uma batalha que durou 10 longos anos", 
-
-"que terminou com uma intervenção dos Deuses...",
-
-"a guerra acabou com a extinção de ambas as raças...",
-
-"porém com a destruição e o caos...", 
-
-"quando não parecia ter mais esperança para o mundo...",
-
-"o mundo estava em ruinas, e não existia", 
-
-"mais nenhuma raça para viver nele...",
-
-"uma nova raça, chamada de Dhameri,", 
-
-"filhos de Alhtrora, foi trazida ao mundo...",
-
-" hoje no ano 2000 d.R, o mundo está a beira de um novo caos...",
-
-"um grupo chamado Cavaleiros da penumbra,", 
-
-"está buscando reviver Iksu, e liberar Zul'thum...",
-
-"Para iniciar um reinado de Caos...",
-
-"resta apenas uma esperança para o mundo...", 
-
-"Você...", 
-
-"Heroi é seu dever acabar com os cavaleiros da penumbra", 
-
-"e impedir que eles revivam Iksu...",
-
-"Salve o mundo da escuridão eterna...",
-"Não falhe, pois essa é a única esperança..."
-]
 
 let map = [
     [18,18,18,18,18,18,18,18,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -347,8 +154,8 @@ const cam  = {
 };
 
 const player = {
-    X: 150,
-    Y: 100,
+    X: 300,
+    Y: 150,
     width: 33,
     height: 32,
     walking: false,
@@ -486,18 +293,6 @@ function draw(){
     movimento();
     drawPlayer();
 }
-if(menuGame && introText){
-    setInterval(()=>{
-        ctx.fillStyle="#000000";
-        ctx.fillRect(0,0, canvas.width, canvas.height)
-        ctx.fillStyle="#FFFFFF";
-        ctx.font = "25px comic sans"
-        ctx.fillText(`${gameIntro[introCount]}`, 10, 550);
-        console.log(introCount, menuGame)
-        introCount++
-        if(introCount > gameIntro.length) introText = false
-    }, 6000)
-} else {
     setInterval( ()=>{
         if(menuGame && introText == false){
             drawTelaInicial();
@@ -509,7 +304,6 @@ if(menuGame && introText){
             }
         }
     }, 0.2);
-}
 
 function movimento(){
     if (player.frameX < 3 && player.walking) player.frameX ++ 
